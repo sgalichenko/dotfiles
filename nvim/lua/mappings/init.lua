4a97e1ca1cc3170064c601f444a86b93e4ba7d23
Semyon Galichenko <gyroflop@gmail.com>
2022-07-13 10:11:45 +0900

    Switch nvim configs to lua

---
+++ nvim/lua/mappings/init.lua
@@ -0,0 +1,24 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = ' '
+
+-- Close buffer
+map('n', '<leader>x', ':bd<CR>', opts)
+
+-- Copy/paste to/from system clipboard
+map('v', '<leader>y', '"+y', opts)
+map('v', '<leader>Y', '"+Y', opts)
+map('v', '<leader>p', '"+p', opts)
+map('v', '<leader>P', '"+P', opts)
+
+-- Moving highlighted text around
+map('v', 'J', ":m '>+1<CR>gv=gv", opts)
+map('v', 'K', ":m '<-2<CR>gv=gv", opts)
+
+-- Fast comment / uncomment
+map('v', '#', ':Commentary<CR>', opts)
+
+-- Keep selection after indentation
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
