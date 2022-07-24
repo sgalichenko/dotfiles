9d0fe53267e330b28eb856e52c38241053b51fcc
Semyon Galichenko <gyroflop@gmail.com>
2022-07-24 15:42:29 +0900

    Refactor ansible tasks

--- ansible/config.sh
+++ ansible/config.sh
@@ -3,5 +3,5 @@
 yes | pacman -Syu
 yes | pacman -Sy git
 yes | pacman -Sy ansible
-git clone https://github.com/galichenkosemyon/dotfiles.git /tmp/dotfiles
+git clone https://github.com/sgalichenko/dotfiles.git /tmp/dotfiles
 ansible-playbook /tmp/dotfiles/ansible/configure.yml && rm -R /tmp/dotfiles
