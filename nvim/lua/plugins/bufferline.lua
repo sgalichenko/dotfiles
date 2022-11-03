185d7b5dc5668e45cf2cb99d8d187f2adc1b798b
Semyon Galichenko <gyroflop@gmail.com>
2022-11-03 16:12:04 +0900

    Fix deprecated settings

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -5,7 +5,7 @@ require('bufferline').setup {
     right_mouse_command = "bdelete! %d",
     left_mouse_command = "buffer %d",
     middle_mouse_command = nil,
-    indicator_icon = '▎',
+    indicator_style = '▎',
     buffer_close_icon = '',
     modified_icon = '●',
     close_icon = '',
