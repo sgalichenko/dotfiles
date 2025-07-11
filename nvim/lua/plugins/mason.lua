d75cdd20a37553a8c211c18bf0f19479d2e19a1f
Galichenko Semyon <gyroflop@gmail.com>
2025-07-11 12:04:43 +0900

    Switch to Lazy

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,1 +1,4 @@
-require("mason").setup()
+return {
+    "mason-org/mason.nvim",
+    opts = {}
+}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,25 +1,8 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
-map('n', '<leader>f', ':Telescope find_files hidden=true<CR>', opts)
-map('n', '<leader>g', ':Telescope live_grep<CR>', opts)
-map('n', '<leader>b', ':Telescope buffers<CR>', opts)
-map('n', '<leader>h', ':Telescope help_tags<CR>', opts)
-map('n', '<leader>m', ':Telescope marks<CR>', opts)
-map('n', '<leader>l', ':Telescope current_buffer_fuzzy_finder<CR>', opts)
-map('n', '<leader>n', ':Telescope file_browser<CR>', opts)
-map('n', '<leader>s', ':Telescope git_status<CR>', opts)
-
-local actions = require("telescope.actions")
-
-require("telescope").setup({
-    defaults = {
-      prompt_prefix = '  ',
-      selection_caret = ' ',
-      mappings = {
-        i = {
-          ["<esc>"] = actions.close,
-        },
-      },
-    },
-})
+return {
+  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { },
+  config = function()
+    vim.keymap.set('n', '<leader>d', require('telescope.builtin').diagnostics, { desc = 'Show diagnostics' })
+  end
+}
