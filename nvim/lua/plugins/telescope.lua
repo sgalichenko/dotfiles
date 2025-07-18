fb6eab22be490bfabacdd14619c9928e4f8fecf9
Semyon Galichenko <sgalichenko@duck.com>
2025-07-18 14:12:24 +0900

    Update keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,29 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { },
-  config = function()
-    vim.keymap.set('n', '<leader>d', require('telescope.builtin').diagnostics, { desc = 'Show diagnostics' })
-  end
+  opts = {
+    defaults = {
+      prompt_prefix = '  ',
+      selection_caret = ' ',
+    },
+  },
+  config = function(_, opts)
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
+
+    telescope.setup(opts)
+
+    local keymap = vim.keymap.set
+    local km_opts = { noremap = true, silent = true }
+
+    keymap('n', '<leader>f', function() builtin.find_files({ hidden = true }) end, km_opts)
+    keymap('n', '<leader>g', builtin.live_grep, km_opts)
+    keymap('n', '<leader>b', builtin.buffers, km_opts)
+    keymap('n', '<leader>h', builtin.help_tags, km_opts)
+    keymap('n', '<leader>m', builtin.marks, km_opts)
+    keymap('n', '<leader>l', builtin.current_buffer_fuzzy_find, km_opts)
+    keymap('n', '<leader>s', builtin.git_status, km_opts)
+    keymap('n', '<leader>d', builtin.diagnostics, { desc = 'Show diagnostics' })
+  end,
 }
